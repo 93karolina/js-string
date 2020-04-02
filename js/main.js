085e89887c63ec8d9a16363c4861dc588510fef4
Karolina Leczycka <karolina.leczycka93@gmail.com>
2020-04-02 14:03:47 +0200

    który string jest wiekszy?

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let uwielbiam="Uwielbiam JavaScript";
+let jestem="Jestem świetnym programistą";
+function string(uwielbiam, jestem){
+    return uwielbiam=jestem;
+}
+if (uwielbiam.length>jestem.length){
+    console.log(uwielbiam);
+} else if (uwielbiam.length<jestem.length);{
+    console.log(jestem);
+}
+
+    
